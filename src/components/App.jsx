2aaf5cc5ae764dfc237558e8b16ef3f807758631
Taiga KIYOKAWA <taiga.k127@gmail.com>
2019-06-15 23:42:37 +0900

    define status and handleAdd for save todo data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,29 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Form from './Form';
+import List from './List';
 
-export default class App extends Component {
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todo: []
+    };
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  // Save data
+  handleAdd(e) {
+    console.log(e.target.title.value);
+    e.preventDefault();
+  }
+
   render() {
     return (
       <div className="siimple-box siimple--bg-dark">
         <h1 className="siimple-box-title siimple--color-white">React Todo App</h1>
-        <Form/>
+        <Form handleAdd={ this.handleAdd }/>
+        <div className="siimple-rule"></div>
+        <List/>
       </div>
     );
   }
